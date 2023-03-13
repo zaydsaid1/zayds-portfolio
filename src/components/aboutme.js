1f488eb4ccc8e26242a948b8f933c2f40532a044
“Zayd <zsaid735@gmail.com>
2023-03-13 11:33:50 +0000

    local host running

---
+++ src/components/aboutme.js
@@ -0,0 +1,11 @@
+import React from "react";
+import zayd from "../images/zayd.jpg"
+
+
+export default function about () {
+    return (
+        <section id="ABOUT ME">
+            
+        </section>
+    )
+}
